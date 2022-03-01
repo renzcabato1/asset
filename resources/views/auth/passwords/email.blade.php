5457ab3851979651898c9882d68b5e55dfa3a951
renzcabato1 <cabato.renz.renz@gmail.com>
2022-03-01 14:29:01 +0800

    Done

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,51 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+<section class="ftco-section">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6 text-center mb-5">
+                <img alt="image"  src="{{asset('login_css/images/logo.png')}}" style='width:135px;'>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-6 col-lg-4">
+                <div class="login-wrap p-0">
+              <h3 class="mb-4 text-center">{{ __('Reset Password') }}</h3>
+              @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
                 </div>
+              @endif
+              <form method="POST" action="{{ route('password.email') }}"  aria-label="{{ __('Login') }}" onsubmit='show()' >
+                @csrf
+                  <div class="form-group">
+                      <input id="email" autocomplete="off" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus  placeholder="Email">
+                  </div>
+            
+                @if($errors->any())
+                    <div class="form-group alert alert-danger alert-dismissable">
+                        <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
+                        <strong>{{$errors->first()}}</strong>
+                    </div>
+                @endif
+            <div class="form-group">
+                <button type="submit" class="form-control btn btn-primary submit px-3">{{ __('Send Password Reset Link') }}</button>
+            </div>
+            <div class="form-group d-md-flex">
+                <div class="w-50">
+                   
+                    </div>
+                    <div class="w-50 text-md-right">
+                        <a href="{{ route('login') }}" onclick='show()'  style="color: #fff">Back to login page</a>
+                    </div>
+            </div>
+          </form>
+        
+          </div>
             </div>
         </div>
     </div>
-</div>
+</section>
+
 @endsection
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,65 +1,76 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
-
-                        <input type="hidden" name="token" value="{{ $token }}">
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ $email ?? old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+<section class="ftco-section">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6 text-center mb-5">
+                <img alt="image"  src="{{asset('login_css/images/logo.png')}}" style='width:135px;'>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-6 col-lg-4">
+                <div class="login-wrap p-0">
+              <h3 class="mb-4 text-center">{{ __('Reset Password') }}</h3>
+              <form method="POST" action="{{ route('password.update') }}"  aria-label="{{ __('Reset Password') }}" onsubmit='show()' >
+                @csrf
+                <input type="hidden" name="token" value="{{ $token }}">
+                  <div class="form-group">
+                      <input id="email" autocomplete="off" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus  placeholder="Email">
+                  </div>
+            <div class="form-group">
+              <input id="password-field" type="password" class="form-control" name="password" placeholder="Password" required >
+              <span toggle="#password-field" id='no-password' onclick='show_password();' class="fa fa-fw fa-eye field-icon toggle-password"></span>
+            </div>
+            <div class="form-group">
+              <input id="password-confirm" type="password" name="password_confirmation" class="form-control" placeholder="Confirm Password" required >
+              <span toggle="#password-confirm" id='no-password-confirm' onclick='show_password_confirm();' class="fa fa-fw fa-eye field-icon toggle-password"></span>
+            </div>
+            @if($errors->any())
+                <div class="form-group alert alert-danger alert-dismissable">
+                    <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
+                    <strong>{{$errors->first()}}</strong>
                 </div>
+            @endif
+            <div class="form-group">
+                <button type="submit" class="form-control btn btn-primary submit px-3">Sign In</button>
+            </div>
+            <div class="form-group d-md-flex">
+                <div class="w-50">
+                   
+                </div>
+                <div class="w-50 text-md-right">
+                    <a href="{{ route('login') }}" onclick='show()'  style="color: #fff">Back to login page</a>
+                </div>
+            </div>
+          </form>
+        
+          </div>
             </div>
         </div>
     </div>
-</div>
+</section>
+
+<script>
+    function show_password()
+    {
+        $("#no-password").toggleClass("fa-eye fa-eye-slash");
+        var input = $($("#no-password").attr("toggle"));
+        if (input.attr("type") == "password") {
+        input.attr("type", "text");
+        } else {
+        input.attr("type", "password");
+        }
+    }
+    function show_password_confirm()
+    {
+        $("#no-password-confirm").toggleClass("fa-eye fa-eye-slash");
+        var input = $($("#no-password-confirm").attr("toggle"));
+        if (input.attr("type") == "password") {
+        input.attr("type", "text");
+        } else {
+        input.attr("type", "password");
+        }
+    }
+</script>
 @endsection
