6fb0fb8a8629c2cd9f94a9c6d1f39b4a9be84af1
renzcabato1 <cabato.renz.renz@gmail.com>
2022-06-07 17:21:02 +0800

    fixed error

--- resources/views/available_inventories.blade.php
+++ resources/views/available_inventories.blade.php
@@ -90,5 +90,23 @@
         </div>
     </section>
 </div>
+<script src="{{ asset('assets/bundles/jquery-ui/jquery-ui.min.js') }}"></script>
+<script>
+    $(".custom-switch-input").click(function () {
+      $('#department').empty();
+      if($(this).is(':checked'))
+        {
+          var data = "<label>Department</label><select id='department_select' class='form-control select3' name='department' style='width:100%' required >";
+             data += "<option value=''></option>";
+             data += "@foreach($departments as $department)";
+             data += "<option value='{{$department->code}}'>{{$department->descs}} - {{$department->code}}</option>";
+             data += "@endforeach";
+             data += "</select>";
+             $('#department').append(data);
+             $(".select3").select2();
+        }
+     
+     });
+</script>
 @endsection
 
